f8d080b3ca5e2643949809609354da856c7c8dfc
bubblyba <bubblyba@gmail.com>
2022-06-16 08:28:25 -0700

    Password check complete

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -10,28 +10,37 @@ async function waitForResponse(user) {
 
     })
     .then (response=>response.text())
-    console.log(response)
+    if(response !== "User created"){
+      console.log(response)
+
+    }
+    
+    else{
+    console.log("User created")
+    }
   
   }
 function SignUpForm() {
 
     const[email,setEmail] = useState('')
     const[password,setPassword] = useState('')
+    const[reType,setRetype] = useState('')
+
     var userType = 0
     var errorElement = ""
     var emailError = "";
 
     
     const handleClick=(e)=>{
-      let messages = []
       
-      if(messages.length > 0){
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
-      }
 
-      else{
+      
       e.preventDefault()
+      if(reType !== password){
+        console.log("Passwords must match")
+
+      }
+      else{
       if(document.getElementById('job').checked) {
         userType = 2
         const user = {email,password,userType}
@@ -42,11 +51,14 @@ function SignUpForm() {
         const user = {email,password,userType}
         waitForResponse(user)
       }
-      else{
-        console.log("please fill out user type")
-      }
       
+      else{
+        console.log("Please fill out user type")
       }
+    }
+      
+      
+      
     }
 
     return (
@@ -57,7 +69,7 @@ function SignUpForm() {
               <input type="text" className="emailInput" placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)} ></input>
               {emailError}
               <input type="text" className="passwordInput" placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)} ></input>
-              <input type="text" className="reenterPasswordInput" placeholder="re-enter password" ></input>
+              <input type="text" className="reenterPasswordInput" placeholder="re-enter password" onChange={(e)=>setRetype(e.target.value)}></input>
               <label className="jobButton"><input id="job" type="radio" name="radio" value="2"></input>I want a job</label>
               <label className="hireButton"><input id="hire"type="radio" name="radio" value="1"></input>I want to hire</label>
               <input className="submitButton"type="submit" value="submit" onClick={handleClick}/>
